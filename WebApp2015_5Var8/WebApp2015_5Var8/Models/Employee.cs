6349f20564bc485fccb05c1bba0f0e415270822f
DenisDenisyuk <82416175+DenisDenisyuk@users.noreply.github.com>
2021-12-02 18:54:10 +0300

    Add files via upload

---
+++ WebApp2015_5Var8/WebApp2015_5Var8/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp2015_5Var8.Models
+{
+    public class Employee
+    {
+        // описывает структуру записи о сотруднике
+        public int ID { get; set; }
+        public string Lastname { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string AcademicDegree { get; set; }
+        public string Position { get; set; }
+        public double Salary { get; set; }
+
+        public List<Employee> EmployeeList { get; set; } // список всех сотрудников
+    }
+}
---
+++ WebApp2015_5Var8/WebApp2015_5Var8/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp2015_5Var8.Models
+{
+    public class Student
+    {
+        // описывает структуру записи о студенте
+        public int ID { get; set; }
+        public string Lastname { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CreditCardNumber { get; set; }
+        public int EducationalForm { get; set; }
+        public int BasisOfTraining { get; set; }
+        
+        public List<Student> StudentList { get; set; } // список всех студентов
+    }
+
+}
